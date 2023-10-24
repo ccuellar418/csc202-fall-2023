78e044008405194d2cd4299d2d745b4a425800df
Adan Silva <adan.t.silva77@gmail.com>
2023-10-24 09:29:16 -0700

    More chances

--- Labs/Lab 2/ordered_list_tests.py
+++ Labs/Lab 2/ordered_list_tests.py
@@ -8,13 +8,13 @@ class TestLab4(unittest.TestCase):
         t_list.add(10)
         self.assertEqual(t_list.python_list(), [10])
         self.assertEqual(t_list.size(), 1)
-        # self.assertEqual(t_list.index(10), 0)
-        # self.assertTrue(t_list.search(10))
-        # self.assertFalse(t_list.is_empty())
+        self.assertEqual(t_list.index(10), 0)
+        self.assertTrue(t_list.search(10))
+        self.assertFalse(t_list.is_empty())
         self.assertEqual(t_list.python_list_reversed(), [10])
-        # self.assertTrue(t_list.remove(10))
+        self.assertTrue(t_list.remove(10))
         t_list.add(10)
-        # self.assertEqual(t_list.pop(0), 10)
+        self.assertEqual(t_list.pop(0), 10)
 
 
 if __name__ == "__main__":
